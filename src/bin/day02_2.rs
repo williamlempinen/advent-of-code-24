7a15d33c0f27ac9df65935881f6d61b5e64aa1bd
William Lempinen <wlempin@gmail.com>
2024-12-02 11:59:30 +0200

    part2 done

--- src/bin/day02_2.rs
+++ src/bin/day02_2.rs
@@ -29,41 +29,25 @@ fn read_input_lines(path: &str) -> Result<Vec<Vec<u8>>, io::Error> {
     Ok(results)
 }
 
-fn is_increasing(report: &Vec<u8>) -> (bool, bool) {
-    let mut tolerate = false;
-    println!("STARTING POINT: {:?}, tolerate: {}", report, tolerate);
+fn is_increasing(report: &Vec<u8>) -> bool {
     for idx in 0..report.len() - 1 {
         if report[idx] > report[idx + 1] {
-            if tolerate {
-                return (false, tolerate);
-            } else {
-                println!("UPDATING tolerate");
-                tolerate = true;
-            }
+            return false;
         }
     }
-    (true, tolerate)
+    true
 }
 
-fn is_decreasing(report: &Vec<u8>) -> (bool, bool) {
-    let mut tolerate = false;
-    println!("STARTING POINT: {:?}, tolerate: {}", report, tolerate);
+fn is_decreasing(report: &Vec<u8>) -> bool {
     for idx in 0..report.len() - 1 {
         if report[idx] < report[idx + 1] {
-            if tolerate {
-                return (false, tolerate);
-            } else {
-                println!("UPDATING tolerate");
-                tolerate = true;
-            }
+            return false;
         }
     }
-    (true, tolerate)
+    true
 }
 
-fn valid_adjetant(report: &Vec<u8>, tolerate: bool) -> bool {
-    println!("STARTING POINT: {:?}, tolerate: {}", report, tolerate);
-    let mut local_t = false;
+fn valid_adjetant(report: &Vec<u8>) -> bool {
     let at_most = 3;
     let at_least = 1;
 
@@ -71,45 +55,62 @@ fn valid_adjetant(report: &Vec<u8>, tolerate: bool) -> bool {
         if report[idx].abs_diff(report[idx + 1]) > at_most
             || report[idx].abs_diff(report[idx + 1]) < at_least
         {
-            if tolerate || local_t {
-                println!("NOT VALID ADJENTANT");
-                return false;
-            } else {
-                println!("UPDATING tolerate");
-                local_t = true;
-            }
+            return false;
         }
     }
     true
 }
 
 fn report_is_valid(report: &Vec<u8>) -> bool {
-    let (is_inc, inc_tolerate) = is_increasing(report);
-    let (is_dec, dec_tolerate) = is_decreasing(report);
+    let is_inc = is_increasing(report);
+    let is_dec = is_decreasing(report);
+    let valid_adjetant = valid_adjetant(report);
 
-    if is_inc {
-        return valid_adjetant(report, inc_tolerate);
+    if !is_dec && !is_inc {
+        return false;
     }
 
-    if is_dec {
-        return valid_adjetant(report, dec_tolerate);
+    if !valid_adjetant {
+        return false;
     }
 
+    true
+}
+
+fn use_tolerater(report: &Vec<u8>) -> bool {
+    for idx in 0..report.len() {
+        println!("ORIGINAL: {:?}", report);
+        let mut cloned_report = report.clone();
+        cloned_report.remove(idx);
+        println!("CLONED TO CHECK IF VALID: {:?}", cloned_report);
+        let result = report_is_valid(&cloned_report);
+
+        if result {
+            return true;
+        }
+    }
     return false;
 }
 
 fn main() -> Result<(), String> {
     println!("Day 02!");
 
-    let path = "./src/resources/day02test";
+    let path = "./src/resources/day02input";
     let reports = read_input_lines(path).unwrap();
     let mut results: Vec<bool> = Vec::new();
 
     for report in reports {
+        println!("REPORT: {:?}", report);
         let result = report_is_valid(&report);
-        println!("RES: {result}");
 
-        if result {
+        if !result {
+            println!("WAS NOT VALID LETS USE TOLERATOR");
+            let is_tolerated_result = use_tolerater(&report);
+
+            if is_tolerated_result {
+                results.push(result);
+            }
+        } else {
             results.push(result);
         }
     }
